762ce992e208efabc6baeb8867f3e7aec96bbf6b
FloEll <48258911+FloEll@users.noreply.github.com>
2022-04-06 16:25:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,17 @@ Created on Tue Apr  5 14:42:23 2022
 
 import setuptools
 
-
 setuptools.setup(name='harvest-indices',
       version='0.0.2',
       description='Calculate harvest anomalies from historical harvest data.',
       author='Florian Ellsäßer',
       author_email='info@cropdata.de',
+      url='cropdata.de',
+      project_urls={
+        'Documentation': 'https://github.com/FloEll/harvest-indices/blob/main/README.md',
+        'Source': 'https://github.com/FloEll/harvest-indices',
+        'Tracker': 'https://github.com/FloEll/harvest-indices/issues',
+      },
       license='MIT',
       packages=['harvest_indices'],
       install_requires=[
